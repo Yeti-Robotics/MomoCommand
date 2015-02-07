848a997828e8ef03657d54649ba1bb6a57f557ab
wazateer1 <wazateer1@gmail.com>
2015-02-07 09:56:21 -0500

    - Will not work, Momo  must be deployed to in NetBeans, which does not contain the scheduler, command, or other classes required to run a command based robot. Neither does CRio. Recommend delete repo.

--- src/edu/wpi/first/wpilibj/templates/commands/UserDriveCommand.java
+++ src/edu/wpi/first/wpilibj/templates/commands/UserDriveCommand.java
@@ -18,6 +18,7 @@ public class UserDriveCommand extends Command {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         requires(RobotTemplate.drive);
+
     }
 
     // Called just before this Command runs the first time
